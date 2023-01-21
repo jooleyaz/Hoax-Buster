57b334b61940979fcd2d53d4887488421642ae2e
Jacob Wu <99063771+wu-jacob@users.noreply.github.com>
2023-01-21 13:11:07 -0800

    added home page, explore page and navigation

---
+++ front_end/memory_museum/lib/explore.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ExplorePage extends StatefulWidget {
+  const ExplorePage({super.key});
+
+  @override
+  State<ExplorePage> createState() => ExplorePageState();
+}
+
+class ExplorePageState extends State<ExplorePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Explore Page uwu"),
+    );
+  }
+}
---
+++ front_end/memory_museum/lib/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:memory_museum/explore.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Homepage"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ExplorePage()),
+              );
+            },
+            child: const Text("Explore"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ExplorePage()),
+              );
+            },
+            child: const Text("Explore"),
+          )
+        ]
+      ),
+    );
+  }
+}
